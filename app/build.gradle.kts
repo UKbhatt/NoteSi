d7894e3d53017f71c7f0072ff774ab888c40a29b
Utkarsh Bhatt <ubhatt2004@gmail.com>
2025-07-02 23:23:15 +0530

    minor changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,11 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation "androidx.recyclerview:recyclerview:1.3.1"
+    implementation "androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0"
+    implementation "androidx.lifecycle:lifecycle-livedata-ktx:2.7.0"
+    implementation "androidx.room:room-runtime:2.6.1"
+    kapt "androidx.room:room-compiler:2.6.1"
+    implementation "com.google.android.material:material:1.12.0"
+
 }
